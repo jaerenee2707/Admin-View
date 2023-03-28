8c46b0735158a6e11fe825c95287285230123030
jaerenee2707 <106774773+jaerenee2707@users.noreply.github.com>
2023-03-28 13:46:53 -0500

    Create AdminView.aspx.cs

---
+++ AdminView.aspx.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication1 {
+    public partial class AdminView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dob.Attributes.Add("placeholder", "yyyy-mm-dd");
+            dob.Attributes.Add("type", "date");
+            dob.Attributes.Add("onkeydown", "return false");
+            welcomeHeader.InnerText = "Welcome, Administrator!";
+
+        }
+
+        protected void ButtonAP_Click(object sender, EventArgs e){
+            Response.Redirect("NewPersonnel.aspx?");
+        }
+        protected void ButtonRP_Click(object sender, EventArgs e){
+            Response.Redirect("RemPersonnel.aspx?");
+        }
+        protected void ButtonAO_Click(object sender, EventArgs e){
+            Response.Redirect("NewOffice.aspx?");
+        }
+        protected void ButtonRO_Click(object sender, EventArgs e){
+            Response.Redirect("RemOffice.aspx?");
+        }
+    }
+}
