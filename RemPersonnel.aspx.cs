d3bcdcbba7f1150df4960612c474a6055a992d0c
jaerenee2707 <106774773+jaerenee2707@users.noreply.github.com>
2023-04-13 19:15:38 -0500

    Create RemPersonnel.aspx.cs

---
+++ RemPersonnel.aspx.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication1
+{
+    public partial class RemPersonnel : System.Web.UI.Page
+    {
+        private string adminID;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            adminID = Request.QueryString["adminID"];
+        }
+        protected void SUBMIT_Click_RemPer(object sender, EventArgs e)
+        {
+            string connString = "Server=medicaldatabase3380.mysql.database.azure.com;Database=medicalclinicdb2;Uid=dbadmin;Pwd=Medical123!;";
+            MySqlConnection connection = new MySqlConnection(connString);
+
+            connection.Open();
+
+            try
+            {
+                 //  remove a Personnel
+                if (DropDownList2.SelectedValue == "Doctor")
+                {
+
+                    string query = "SELECT CONCAT('Dr. ', fname, ' ', lname, ' - ', specialty) from doctor WHERE DoctorID = @ID";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@ID", perID.Text);
+                    object result = cmd.ExecuteScalar();
+                    string outcome = result.ToString();
+                    alert("DELETED " + outcome);
+
+
+                    string sql = "DELETE FROM doctor WHERE DoctorID = @ID";
+                    MySqlCommand command = new MySqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@ID", perID.Text);
+                    command.ExecuteNonQuery();
+                }
+                if (DropDownList2.SelectedValue == "Nurse")
+                {
+
+                    string query = "SELECT CONCAT( fname, ' ', lname) FROM nurse WHERE NID = @ID";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@ID", perID.Text);
+                    object result = cmd.ExecuteScalar();
+                    string outcome = result.ToString();
+                    alert("DELETED " + outcome);
+
+                    string sql = "DELETE FROM nurse WHERE NID = @ID";
+                    MySqlCommand command = new MySqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@ID", perID.Text);
+                    command.ExecuteNonQuery();
+                }
+                if (DropDownList2.SelectedValue == "Staff")
+                {
+
+                    string query = "SELECT CONCAT( fname, ' ', lname) FROM staff WHERE StaffID = @ID";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@ID", perID.Text);
+                    object result = cmd.ExecuteScalar();
+                    string outcome = result.ToString();
+                    alert("DELETED " + outcome);
+
+                    string sql = "DELETE FROM staff WHERE StaffID = @ID";
+                    MySqlCommand command = new MySqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@ID", perID.Text);
+                    command.ExecuteNonQuery();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error: " + ex.Message + '\n');
+            }
+
+            connection.Close();
+        }
+        protected void ButtonClear_Click(object sender, EventArgs e)
+        {
+            perID.Text = "";
+        }
+        protected void ButtonExit_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AdminView.aspx?adminID=" + adminID);
+        }
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
